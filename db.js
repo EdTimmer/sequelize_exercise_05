825488b2b959a1bc99bd8da2cc262a2974ee4c6f
Edward Timmer <edtimmer@gmail.com>
2018-06-06 23:07:11 -0400

    commit

--- db.js
+++ db.js
@@ -1,54 +1,103 @@
-// three users ‘moe’, ‘larry’, and ‘curly’
-// cities nyc, la, chicago
-// moe is the mayor of la and larry is the mayor of chicago…
-// but.. also each lives in a city and of course a city can have many users…
-// it makes sense that moe lives in la and larry lives in chicago…
-// give it a shot… it’s not too different from my solution… but try it from scratch…
-// when we are done we are going to make it a requirement that you can’t become mayor of a city unless you live there…
-// but don’t worry about that now…
+/*USERS APARTMENTS BUILDINGS
+Users are: 'Moe', 'Larry', and 'Curly'.
+Buildings are: '382 CPW', '666 Fifth Ave', and 'Faulty Towers'.
+A user has a name and a building has a name.
+A user can have an apartment in one or more buildings.
+An apartment has a number.
+Moe has Apt. 6K at 382 CPW
+Moe also has Penthouse at 666 5th Ave.
+Larry has Apt. 1C in Faulty Towers.
+*/
 
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/my_db');
 
-const User = conn.define('user', {
+const Building = conn.define('building', {
   name: Sequelize.STRING
-}, {
-  hooks: {
-    beforeValidate: function(user) {
-      if (user.cityId !== user.mayorId) {
-        user.mayorId = null;
-      }
-    }
-  }
 });
 
-const City = conn.define('city', {
+const Apartment = conn.define('apartment', {
   name: Sequelize.STRING
+});
+
+const User = conn.define('user', {
+  name: Sequelize.STRING,
 });
 
 const usernames = ['Moe', 'Larry', 'Curly'];
-const citynames = ['New York City', 'Los Angeles', 'Chicago'];
+const buildingnames = ['382 CPW', '666 Fifth Ave', 'Faulty Towers'];
+const apartmentnames = ['6K', 'penthouse', '1C'];
 
-User.belongsTo(City);
-User.belongsTo(City, { as: 'mayor' });
-City.hasMany(User);
-City.hasOne(User, { as: 'mayor_of', foreignKey: 'mayorId'});
+Apartment.belongsTo(Building);  //apartment has buildingId
+Building.hasMany(Apartment, {as: 'building_of', foreignKey: 'buildingId'});
+Apartment.belongsTo(User);      //apartment has userId
+User.hasMany(Apartment);
 
+const seed = () => { 
+  const buildingPromises = Promise.all(buildingnames.map(name => Building.create({ name })));
+  const userPromises = Promise.all(usernames.map(name => User.create({ name })));
+  const apartmentPromises = Promise.all(apartmentnames.map(name => Apartment.create({ name })));
+  
+  return Promise.all([buildingPromises, userPromises, apartmentPromises])
+    .then(([buildings, users, apartments]) => {
+      const [cpw, fifthAve, faulty] = buildings;
+      const [moe, larry, curly] = users;
+      const [sixK, ph, oneC] = apartments;
+      return Promise.all([
+        sixK.setUser(moe),
+        ph.setUser(moe),
+        oneC.setUser(larry),
+        sixK.setBuilding(cpw),
+        ph.setBuilding(fifthAve),
+        oneC.setBuilding(faulty)
+      ]);
+    });
+  }
+
+//FIND LARRY'S APARTMENT AND BUILDING
 conn.sync({ force: true })
-  .then(() => {
-    const userPromises = Promise.all(usernames.map(name => User.create({ name })));
-    const cityPromises = Promise.all(citynames.map(name => City.create({ name })));
-    return Promise.all([userPromises, cityPromises]);
+  .then(() => seed())
+  .then(()=> User.findOne({
+    where: {
+      name: 'Larry'
+    }
+  }))
+  .then(user => {
+    _user = user;
+    return Apartment.findOne({
+      where: {
+        userId: user.id
+      }
+    })
   })
-  .then(([users, cities]) => {
-    const [moe, larry, curly] = users;
-    const [nyc, la, chicago] = cities;
-    return Promise.all([
-      moe.setCity(la),
-      larry.setCity(chicago),
-      curly.setCity(nyc),
-      moe.setMayor(la),
-      larry.setMayor(chicago),
-      curly.setMayor(la)  //intentional wrong city
-    ]);
+  .then(apt => {
+    _apt = apt;
+    return Building.findById(apt.buildingId)
+  })
+  .then(building => console.log(`${_user.name} lives in apartment ${_apt.name} in ${building.name}`));
+
+//FIND ALL OF MOE'S BUILDINGS
+conn.sync({ force: true })
+  .then(() => seed())
+  .then(()=> User.findOne({
+    where: {
+      name: 'Moe'
+    }
+  }))
+  .then(user => {
+    _user = user;
+    return Apartment.findAll({
+      where: {
+        userId: user.id
+      }
+    })
+  })
+  .then(apts => {
+    _apts = apts;
+    apts.forEach( apt => Building.findAll({
+      where: {
+        id: apt.buildingId
+      }
+    })
+    .then(buildings => buildings.forEach(building => console.log(building.name))))
   });
